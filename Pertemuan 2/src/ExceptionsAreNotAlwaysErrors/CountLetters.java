8ff222cb36d34e437ea466463eae9012df3dc176
johansalusu <54470577+johansalusu@users.noreply.github.com>
2021-03-22 22:45:23 +0700

    Exceptions Aren't Always Errors

---
+++ Pertemuan 2/src/ExceptionsAreNotAlwaysErrors/CountLetters.java
@@ -0,0 +1,51 @@
+package ExceptionsAreNotAlwaysErrors;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Johan Salusu
+ */
+
+// ****************************************************************
+// CountLetters.java
+//
+// Reads a words from the standard input and prints the number of
+// occurrences of each letter in that word.
+//
+// ****************************************************************
+import java.util.Scanner;
+public class CountLetters
+{
+    public static void main(String[] args)
+    {
+        int[] counts = new int[26];
+        Scanner scan = new Scanner(System.in);
+        
+        //get word from user
+        System.out.print("Enter a single word (letters only, please!): ");
+        String word = scan.nextLine();
+        
+        //convert to all upper case
+        word = word.toUpperCase();
+        
+        try{
+        //count frequency of each letter in string
+        for (int i=0; i < word.length(); i++)
+            counts[word.charAt(i)-'A']++;
+        }
+        catch(ArrayIndexOutOfBoundsException exception){
+            System.out.println("There's an error in array index.");
+        }
+        
+        //print frequencies
+        System.out.println();
+        for (int i=0; i < counts.length; i++)
+            if (counts [i] != 0)
+                System.out.println((char)(i +'A') + ": " + counts[i]);
+    }
+}
