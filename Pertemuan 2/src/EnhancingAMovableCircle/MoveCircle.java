f182401934a18a372e7a73bb62d6a7d3c2aa4c95
johansalusu <54470577+johansalusu@users.noreply.github.com>
2021-03-26 18:00:33 +0700

    Enhancing a Movable Circle

--- Pertemuan 2/src/EnhancingAMovableCircle/MoveCircle.java
+++ Pertemuan 2/src/EnhancingAMovableCircle/MoveCircle.java
@@ -26,7 +26,7 @@ public class MoveCircle
     {
         JFrame frame = new JFrame ("MoveCircle");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400,300);
+        frame.setSize(480, 430);
         
         frame.getContentPane().add(new CirclePanel(400,300));
         frame.setVisible(true);
